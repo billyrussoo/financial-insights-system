70a4ca5f87a38f1fde55a7769f4efa2fff3247ae
Muhammad Mamoon Abbasi <80235793+billyrussoo@users.noreply.github.com>
2025-05-13 22:51:30 +0500

    Update main.py

--- src/api/main.py
+++ src/api/main.py
@@ -72,5 +72,5 @@ if __name__ == "__main__":
     import uvicorn
     import os
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("api.main:app", host="0.0.0.0", port=port)
 
